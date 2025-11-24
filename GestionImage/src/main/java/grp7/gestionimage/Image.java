be232b7b33af28f53caae6a362e8ebe4ac141d6e
Victor Duforest <vduforest01@gmail.com>
2025-11-24 16:40:31 +0100

    Ajout classe Image

---
+++ GestionImage/src/main/java/grp7/gestionimage/Image.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grp7.gestionimage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vdufo
+ */
+public class Image {
+    private int hauteur;
+    private int largeur;
+    private String commentaire;
+    private ArrayList listeCases;
+
+    private final int valeurMax=255;
+    
+    
+}
