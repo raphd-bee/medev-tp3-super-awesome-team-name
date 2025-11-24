1400027487f63967876f4f1606b386f44fc071dc
raphd-bee <raph.delacote@gmail.com>
2025-11-24 17:57:16 +0100

    Modify main method to read and write image files
    Updated main method to handle IOException and added image reading and writing functionality.

--- GestionImage/src/main/java/grp7/gestionimage/GestionImage.java
+++ GestionImage/src/main/java/grp7/gestionimage/GestionImage.java
@@ -4,13 +4,21 @@
 
 package grp7.gestionimage;
 
+import java.io.IOException;
+
 /**
  *
  * @author vdufo
  */
 public class GestionImage {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Image img = new Image();
+        String txtFilePath;
+        
+        txtFilePath="C:\\Users\\rapha\\Desktop\\INFOSI\\MEDEV\\ImagesTestPGM\\ImagesTestPGM\\baboon.pgm";
+        img.lireImage(txtFilePath);
+        System.out.println(img);
+        img.ecritureImage();
     }
 }
