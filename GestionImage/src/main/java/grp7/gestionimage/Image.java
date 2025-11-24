cf8482f19fd5a142dc16811e0ec5b31e1f553abe
Victor Duforest <vduforest01@gmail.com>
2025-11-24 16:46:04 +0100

    Modif classe Image, début méthode ecriture

--- GestionImage/src/main/java/grp7/gestionimage/Image.java
+++ GestionImage/src/main/java/grp7/gestionimage/Image.java
@@ -15,8 +15,62 @@ public class Image {
     private int largeur;
     private String commentaire;
     private ArrayList listeCases;
-
+    private ArrayList listeFinLigne;
+            
     private final int valeurMax=255;
+
+    public Image(int hauteur, int largeur, String commentaire, ArrayList listeCases, ArrayList listeFinLigne) {
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+        this.commentaire = commentaire;
+        this.listeCases = listeCases;
+        this.listeFinLigne = listeFinLigne;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public ArrayList getListeCases() {
+        return listeCases;
+    }
+
+    public void setListeCases(ArrayList listeCases) {
+        this.listeCases = listeCases;
+    }
+
+    public ArrayList getListeFinLigne() {
+        return listeFinLigne;
+    }
+
+    public void setListeFinLigne(ArrayList listeFinLigne) {
+        this.listeFinLigne = listeFinLigne;
+    }
     
-    
+    public void ecritureImage(){
+        // Creation d'un fichier texte
+        
+        //
+    }
 }
+ 
