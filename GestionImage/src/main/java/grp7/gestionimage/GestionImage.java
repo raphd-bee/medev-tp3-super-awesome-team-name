ad97e5a600edc15a214b3e24c741ab9134d54551
Victor Duforest <vduforest01@gmail.com>
2025-11-24 16:23:30 +0100

    Merge origin/main into main
    # Conflicts:
    #	Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Case.java
    #	Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/CaseAchetable.java

---
+++ GestionImage/src/main/java/grp7/gestionimage/GestionImage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grp7.gestionimage;
+
+/**
+ *
+ * @author vdufo
+ */
+public class GestionImage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
