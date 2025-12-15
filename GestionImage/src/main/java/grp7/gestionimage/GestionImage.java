f4882d54292bdff0bfac0c06c8fa1c115af37ba2
raphd-bee <raph.delacote@gmail.com>
2025-12-15 15:00:06 +0100

    Update GestionImage.java

--- GestionImage/src/main/java/grp7/gestionimage/GestionImage.java
+++ GestionImage/src/main/java/grp7/gestionimage/GestionImage.java
@@ -12,13 +12,5 @@ import java.io.IOException;
  */
 public class GestionImage {
 
-    public static void main(String[] args) throws IOException {
-        Image img = new Image();
-        String txtFilePath;
-        
-        txtFilePath="C:\\Users\\rapha\\Desktop\\INFOSI\\MEDEV\\ImagesTestPGM\\ImagesTestPGM\\baboon.pgm";
-        img.lireImage(txtFilePath);
-        System.out.println(img);
-        img.ecritureImage();
-    }
+    
 }
